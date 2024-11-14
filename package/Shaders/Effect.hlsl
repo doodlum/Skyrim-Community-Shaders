9cb8d81c762178fd0da536ec94951acf91f34f43
doodlum <doodlum@users.noreply.github.com>
2024-11-14 13:17:32 +0000

    style: 🎨 apply clang-format changes

--- package/Shaders/Effect.hlsl
+++ package/Shaders/Effect.hlsl
@@ -532,11 +532,11 @@ float3 GetLightingColor(float3 msPosition, float3 worldPosition, float4 screenPo
 		color += dirLightColor * GetEffectShadow(worldPosition, normalize(worldPosition), screenPosition, eyeIndex);
 	}
 
-#	if !defined(LIGHT_LIMIT_FIX)
+#		if !defined(LIGHT_LIMIT_FIX)
 	color.x += dot(PLightColorR * lightFadeMul, 1.0.xxxx);
 	color.y += dot(PLightColorG * lightFadeMul, 1.0.xxxx);
 	color.z += dot(PLightColorB * lightFadeMul, 1.0.xxxx);
-#	endif
+#		endif
 
 	return color;
 }
