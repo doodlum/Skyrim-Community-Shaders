26858bb0f17f1046971d7687eb7a27f3e5ed9fa9
Tim <15017472+doodlum@users.noreply.github.com>
2024-11-23 23:01:34 +0000

    chore: remove redundant code

--- package/Shaders/Effect.hlsl
+++ package/Shaders/Effect.hlsl
@@ -533,16 +533,13 @@ float3 GetLightingColor(float3 msPosition, float3 worldPosition, float4 screenPo
 	}
 
 #		if defined(LIGHT_LIMIT_FIX)
-	if (!(ExtraShaderDescriptor & ExtraFlags::InWorld)){
+	if (!(ExtraShaderDescriptor & ExtraFlags::InWorld))
+#		endif
+	{
 		color.x += dot(PLightColorR * lightFadeMul, 1.0.xxxx);
 		color.y += dot(PLightColorG * lightFadeMul, 1.0.xxxx);
 		color.z += dot(PLightColorB * lightFadeMul, 1.0.xxxx);
 	}
-#		else
-	color.x += dot(PLightColorR * lightFadeMul, 1.0.xxxx);
-	color.y += dot(PLightColorG * lightFadeMul, 1.0.xxxx);
-	color.z += dot(PLightColorB * lightFadeMul, 1.0.xxxx);
-#		endif
 
 	return color;
 }
