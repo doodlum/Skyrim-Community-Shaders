759a91fd79aa2f951a2b22123a8d6167a9043c13
Tim <15017472+doodlum@users.noreply.github.com>
2024-12-06 21:02:13 +0000

    refactor: adjust brackets in VL

--- src/Features/VolumetricLighting.cpp
+++ src/Features/VolumetricLighting.cpp
@@ -44,9 +44,8 @@ void VolumetricLighting::RestoreDefaultSettings()
 {
 	settings = {};
 	Util::ResetGameSettingsToDefaults(VLSettings);
-	if (State::GetSingleton()->isVR) {
+	if (State::GetSingleton()->isVR)
 		Util::ResetGameSettingsToDefaults(hiddenVRSettings);
-	}
 }
 
 void VolumetricLighting::DataLoaded()
