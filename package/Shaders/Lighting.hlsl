04d03799d9f33d549244254a1d42408cedd54ccb
doodlum <doodlum@users.noreply.github.com>
2025-01-03 14:38:44 +0000

    style: 🎨 apply clang-format changes

--- package/Shaders/Lighting.hlsl
+++ package/Shaders/Lighting.hlsl
@@ -2328,13 +2328,12 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace
 	bool dynamicCubemap = false;
 	if (envMask > 0.0 && envSize.x == 1 && envSize.y == 1) {
 		dynamicCubemap = true;
-		
+
 		// Dynamic Cubemap Creator sets this value to black, if it is anything but black it is wrong
 		float3 envColorTest = TexEnvSampler.SampleLevel(SampEnvSampler, float3(0.0, 1.0, 0.0), 15);
 		dynamicCubemap = all(envColorTest == 0.0);
 
-		if (dynamicCubemap)
-		{
+		if (dynamicCubemap) {
 			envColorBase = TexEnvSampler.SampleLevel(SampEnvSampler, float3(1.0, 0.0, 0.0), 15);
 
 			if (envColorBase.a < 1.0) {
