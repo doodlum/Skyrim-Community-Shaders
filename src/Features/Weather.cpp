63bfb860605b1ae31e5a138cea671adbfc9eb8d4
Tim <15017472+doodlum@users.noreply.github.com>
2025-02-06 02:05:35 +0000

    feat: dalc baking thing

--- src/Features/Weather.cpp
+++ src/Features/Weather.cpp
@@ -3,6 +3,65 @@
 #include "Deferred.h"
 #include "Util.h"
 
+static RE::Color dalcColors[6];
+
+void Weather::DrawSettings()
+{
+	if (ImGui::Button("Update DALC values")) {
+		auto& context = State::GetSingleton()->context;
+
+		// Copy the cubemap to the staging texture
+		context->CopyResource(diffuseIBLCubemapCPUTexture->resource.get(), diffuseIBLCubemapTexture->resource.get());
+
+		// Read back data from each face (6 faces)
+		for (UINT face = 0; face < 6; ++face) {
+			D3D11_MAPPED_SUBRESOURCE mappedResource = {};
+			auto hr = context->Map(diffuseIBLCubemapCPUTexture->resource.get(), face, D3D11_MAP_READ, 0, &mappedResource);
+			if (SUCCEEDED(hr)) {
+				// Assuming format is DXGI_FORMAT_R8G8B8A8_UNORM (adjust based on your format)
+				uint32_t* pixelData = reinterpret_cast<uint32_t*>(mappedResource.pData);
+
+				uint8_t r = (*pixelData >> 0) & 0xFF;
+				uint8_t g = (*pixelData >> 8) & 0xFF;
+				uint8_t b = (*pixelData >> 16) & 0xFF;
+
+				dalcColors[face] = RE::Color{ r, g, b, 0 };
+
+				context->Unmap(diffuseIBLCubemapCPUTexture->resource.get(), face);
+			}
+		}
+	}
+
+	ImGui::Text("DALC values");
+
+	std::string faceStr[6]{ "X+", "X-", "Y+", "Y-", "Z+", "Z-" };
+
+	for (UINT face = 0; face < 6; ++face) {
+
+		std::string rText = std::format("R {}", dalcColors[face].red);
+		std::string gText = std::format("G {}", dalcColors[face].green);
+		std::string bText = std::format("B {}", dalcColors[face].blue);
+
+		ImGui::Text("%s:", faceStr[face].c_str());  // Display the face name
+
+		// Make each color channel selectable and copy its value when clicked
+		if (ImGui::Button(rText.c_str())) {
+			ImGui::SetClipboardText(std::to_string(dalcColors[face].red).c_str());
+		}
+		ImGui::SameLine();
+
+		if (ImGui::Button(gText.c_str())) {
+			ImGui::SetClipboardText(std::to_string(dalcColors[face].green).c_str());
+		}
+		ImGui::SameLine();
+
+		if (ImGui::Button(bText.c_str())) {
+			ImGui::SetClipboardText(std::to_string(dalcColors[face].blue).c_str());
+		}
+		ImGui::Separator();
+	}
+}
+
 void Weather::Bind()
 {
 	if (loaded) {
@@ -10,8 +69,8 @@ void Weather::Bind()
 
 		// Set PS shader resource
 		{
-			ID3D11ShaderResourceView* srv = diffuseIBLTexture->srv.get();
-			context->PSSetShaderResources(80, 1, &srv);
+			ID3D11ShaderResourceView* srv = diffuseIBLCubemapTexture->srv.get();
+			context->PSSetShaderResources(100, 1, &srv);
 		}
 	}
 }
@@ -24,13 +83,13 @@ void Weather::Prepass()
 	auto& reflections = renderer->GetRendererData().cubemapRenderTargets[RE::RENDER_TARGET_CUBEMAP::kREFLECTIONS];
 
 	std::array<ID3D11ShaderResourceView*, 1> srvs = { reflections.SRV };
-	std::array<ID3D11UnorderedAccessView*, 1> uavs = { diffuseIBLTexture->uav.get() };
+	std::array<ID3D11UnorderedAccessView*, 1> uavs = { diffuseIBLCubemapTexture->uav.get() };
 	std::array<ID3D11SamplerState*, 1> samplers = { Deferred::GetSingleton()->linearSampler };
 
 	// Unset PS shader resource
 	{
 		ID3D11ShaderResourceView* srv = nullptr;
-		context->PSSetShaderResources(80, 1, &srv);
+		context->PSSetShaderResources(100, 1, &srv);
 	}
 
 	// IBL
@@ -41,7 +100,7 @@ void Weather::Prepass()
 		context->CSSetShaderResources(0, (uint)srvs.size(), srvs.data());
 		context->CSSetUnorderedAccessViews(0, (uint)uavs.size(), uavs.data(), nullptr);
 		context->CSSetShader(GetDiffuseIBLCS(), nullptr, 0);
-		context->Dispatch(1, 1, 1);
+		context->Dispatch(1, 1, 6);
 	}
 
 	// Reset
@@ -58,7 +117,7 @@ void Weather::Prepass()
 
 	// Set PS shader resource
 	{
-		ID3D11ShaderResourceView* srv = diffuseIBLTexture->srv.get();
+		ID3D11ShaderResourceView* srv = diffuseIBLCubemapTexture->srv.get();
 		context->PSSetShaderResources(100, 1, &srv);
 	}
 }
@@ -97,6 +156,44 @@ void Weather::SetupResources()
 		diffuseIBLTexture->CreateSRV(srvDesc);
 		diffuseIBLTexture->CreateUAV(uavDesc);
 	}
+
+	auto renderer = RE::BSGraphics::Renderer::GetSingleton();
+	auto& cubemap = renderer->GetRendererData().cubemapRenderTargets[RE::RENDER_TARGETS_CUBEMAP::kREFLECTIONS];
+
+	{
+		D3D11_TEXTURE2D_DESC texDesc;
+		cubemap.texture->GetDesc(&texDesc);
+
+		D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
+		cubemap.SRV->GetDesc(&srvDesc);
+
+		texDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		srvDesc.Format = texDesc.Format;
+
+		texDesc.BindFlags |= D3D11_BIND_UNORDERED_ACCESS;
+		texDesc.Width = 1;
+		texDesc.Height = 1;
+
+		// Create additional resources
+
+		D3D11_UNORDERED_ACCESS_VIEW_DESC uavDesc = {};
+		uavDesc.Format = texDesc.Format;
+		uavDesc.ViewDimension = D3D11_UAV_DIMENSION_TEXTURE2DARRAY;
+		uavDesc.Texture2DArray.MipSlice = 0;
+		uavDesc.Texture2DArray.FirstArraySlice = 0;
+		uavDesc.Texture2DArray.ArraySize = texDesc.ArraySize;
+
+		diffuseIBLCubemapTexture = new Texture2D(texDesc);
+		diffuseIBLCubemapTexture->CreateSRV(srvDesc);
+		diffuseIBLCubemapTexture->CreateUAV(uavDesc);
+
+		texDesc.Usage = D3D11_USAGE_STAGING;
+		texDesc.BindFlags = 0;
+		texDesc.CPUAccessFlags = D3D11_CPU_ACCESS_READ;
+		texDesc.MiscFlags = 0;  // Remove cubemap flag
+
+		diffuseIBLCubemapCPUTexture = new Texture2D(texDesc);
+	}
 }
 
 void Weather::ClearShaderCache()
@@ -109,6 +206,6 @@ void Weather::ClearShaderCache()
 ID3D11ComputeShader* Weather::GetDiffuseIBLCS()
 {
 	if (!diffuseIBLCS)
-		diffuseIBLCS = static_cast<ID3D11ComputeShader*>(Util::CompileShader(L"Data\\Shaders\\Weather\\DiffuseIBLCS.hlsl", {}, "cs_5_0"));
+		diffuseIBLCS = static_cast<ID3D11ComputeShader*>(Util::CompileShader(L"Data\\Shaders\\Weather\\IrmapCS.hlsl", {}, "cs_5_0"));
 	return diffuseIBLCS;
 }
--- src/Features/Weather.h
+++ src/Features/Weather.h
@@ -17,8 +17,13 @@ public:
 	virtual inline std::string GetShortName() override { return "Weather"; }
 	virtual inline std::string_view GetShaderDefineName() override { return "WEATHER"; }
 
+	Texture2D* diffuseIBLCubemapTexture = nullptr;
+	Texture2D* diffuseIBLCubemapCPUTexture = nullptr;
+
 	Texture2D* diffuseIBLTexture = nullptr;
 	ID3D11ComputeShader* diffuseIBLCS = nullptr;
+
+	virtual void DrawSettings() override;
 
 	void Bind();
 
