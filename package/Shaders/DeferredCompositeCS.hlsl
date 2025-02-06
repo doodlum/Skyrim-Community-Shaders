b4bb98752b43b5e0f20f90c3cc7eabae52c10da7
doodlum <doodlum@users.noreply.github.com>
2025-02-06 17:21:08 +0000

    style: 🎨 apply clang-format changes

--- package/Shaders/DeferredCompositeCS.hlsl
+++ package/Shaders/DeferredCompositeCS.hlsl
@@ -137,7 +137,7 @@ void SampleSSGISpecular(uint2 pixCoord, sh2 lobe, out float ao, out float3 il)
 
 		if (skylightingSpecular > 0.0)
 			specularIrradianceReflections = EnvReflectionsTexture.SampleLevel(LinearSampler, R, level);
-		
+
 		finalIrradiance = finalIrradiance * skylightingSpecular + lerp(specularIrradiance, specularIrradianceReflections, skylightingSpecular);
 #	else
 		float3 specularIrradianceReflections = EnvReflectionsTexture.SampleLevel(LinearSampler, R, level);
