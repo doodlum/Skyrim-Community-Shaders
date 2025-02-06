e0c3bf40ec09c69e53736a0cddcad72859a8ee4d
maria <mariatkins86@gmail.com>
2025-02-06 23:30:15 +0000

    refactor: remove testing preview window

--- src/Editor/EditorWindow.h
+++ src/Editor/EditorWindow.h
@@ -25,26 +25,11 @@ public:
 	std::vector<Widget*> worldSpaceWidgets;
 	std::vector<Widget*> lightingTemplateWidgets;
 
-	RE::TESWeather* originalWeather;
-
-	WeatherWidget* preview;
-	WeatherWidget* previewWeather2;
-
-	float lerpValue;
-	
-	std::string weatherName1 = "Select weather";
-	std::string weatherName2 = "Select weather";
-
-
 	void ShowObjectsWindow();
 
 	void ShowViewportWindow();
 
 	void ShowWidgetWindow();
-
-	void ShowPreviewWindow();
-
-	void DisplayDALC(std::string, RE::BGSDirectionalAmbientLightingColors::Directional::MaxMin<RE::Color>*);
 
 	void RenderUI();
 
