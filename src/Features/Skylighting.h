134ccda8fcf78f99bdbf0f902e4630e1a132ff7d
Tim <15017472+doodlum@users.noreply.github.com>
2025-02-19 03:57:49 +0000

    chore: lower skylighting amount

--- src/Features/Skylighting.h
+++ src/Features/Skylighting.h
@@ -34,7 +34,7 @@ struct Skylighting : Feature
 	struct Settings
 	{
 		float MaxZenith = 3.1415926f / 2.f;  // 90 deg
-		float MinDiffuseVisibility = 0.2f;
+		float MinDiffuseVisibility = 0.1f;
 		float MinSpecularVisibility = 0.1f;
 		float SSGIAmbientDimmer = 1.0f;
 	} settings;
